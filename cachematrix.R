d95283bceebd45c3da128cf92e1f5874827f2cbb
warin-v <warin-v@users.noreply.github.com>
2015-01-20 20:55:16 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## The below function solve for the value of inversed matrix and then store it as a new
+## value in the global environment.
 makeCacheMatrix <- function(x = matrix()) {
-
+        I <- NULL
+        set <- function(y) {
+                x <<- y
+                I <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) I <<- solve
+        getinv <- function() I
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## The next function will get the value of inversed matrix stored in below value.
+cachesolve <- function(x, ...) {
+        I <- x$getinv()
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+        data <- x$get()
+        I <- solve(data)
+        x$setinv(I)
+        I
 }
